390c2c8aa2fe60aee9508c887f2850b153c786de
Rajat-Goyal-Enwidth <rajat.goyal@enwidth.com>
2021-09-18 13:30:18 +0530

    DemoController Added from subbranch 'rajat' just for testing purpose

---
+++ Spring-Boot-MySql-Docker/src/main/java/com/root/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.root.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+	
+	@GetMapping("/rajat")
+	public String sayHiRajat()
+	{
+		return "Hi, Rajat";
+	}
+}
